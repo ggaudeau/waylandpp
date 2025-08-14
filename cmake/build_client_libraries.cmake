47e803b7d630d061cdd21574e98fc607a04e6cd2
Gerard Gaudeau <gerard.gaudeau@yahoo.fr>
2025-08-14 16:09:56 +0200

    [cmake] Added a flag to enable compilation of a waylandpp lib using the wayland WLR protocols

--- cmake/build_client_libraries.cmake
+++ cmake/build_client_libraries.cmake
@@ -125,3 +125,21 @@ define_library(wayland-client-plasma++
   wayland-client-protocol.hpp)
   target_link_libraries(wayland-client-plasma++ INTERFACE wayland-client++)
 endif()
+
+if(INSTALL_WLR_PROTOCOLS)
+  set(PROTO_FILES_WLR
+    "wayland-client-protocol-wlr.hpp"
+    "wayland-client-protocol-wlr.cpp")
+  generate_cpp_client_files("${PROTO_XMLS_WLR}" "${PROTO_FILES_WLR}" "-x;wayland-client-protocol-extra.hpp" "")
+  set(WAYLAND_CLIENT_WLR_HEADERS
+    "${CMAKE_CURRENT_BINARY_DIR}/wayland-client-protocol-extra.hpp"
+    "${CMAKE_CURRENT_BINARY_DIR}/wayland-client-protocol-wlr.hpp")
+  define_library(wayland-client-wlr++
+    "${WAYLAND_CLIENT_CFLAGS}"
+    "${WAYLAND_CLIENT_LIBRARIES};wayland-client-extra++"
+    "${WAYLAND_CLIENT_WLR_HEADERS}"
+    wayland-client-protocol-wlr.cpp 
+    wayland-client-protocol-wlr.hpp
+    wayland-client-protocol.hpp)
+  target_link_libraries(wayland-client-wlr++ INTERFACE wayland-client-extra++)
+endif()
